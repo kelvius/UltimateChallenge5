bb520a5aff2aacc146a016fdca356385f9541cbb
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-02-27 00:21:08 -0600

    Commited fake db using rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+676.times do
+
+  product = Product.new
+
+product.title = Faker::Commerce.product_name #=> "Practical Granite Shirt"
+
+product.price = Faker::Commerce.price #=> 44.6
+
+product.stock_quantity = Faker::Number.number(digits: 2) #=> 0~99
+
+  product.save
+end
