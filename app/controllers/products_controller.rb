2ae41d217bb8fe9a74d975544742c6ac41b72e0d
hambaconsupreme <jvalerio@academic.rrc.ca>
2024-02-28 06:49:31 -0600

    Full App with reference and GET

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
+# WEBD-3008 Full-Stack Web Development
+# John Kelvin Valerio
+# 2021-02-28
+# Ultimate Challenge 5
+
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
